ebf233d281be2a0e83a505a204c5642a6fb89201
João Pedro Perondini <jpfperondini@gmail.com>
2016-12-05 09:18:12 -0200

    Add support to default params on android

--- www/cordovaAddContact.js
+++ www/cordovaAddContact.js
@@ -1,7 +1,25 @@
+cordova.define("cordova-add-contact.cordova-add-contact", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 const emptyCallback = function() {}
 
-exports.addContact = function() {
-    exec(emptyCallback, emptyCallback, "CordovaAddContact", "addContact", []);
+exports.addContact = function(params) {
+    if(typeof params == 'undefined') {
+        params = {}
+    }
+
+    if(typeof params.name == 'undefined' || params.name == null) {
+        params.name = ''
+    }
+
+    if(typeof params.email == 'undefined' || params.email == null) {
+        params.email = ''
+    }
+
+    if(typeof params.phone == 'undefined' || params.phone == null) {
+        params.phone = ''
+    }
+    exec(emptyCallback, emptyCallback, "CordovaAddContact", "addContact", [params]);
 };
+
+});
