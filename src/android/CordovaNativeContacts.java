f8c7bc84cf301839ecfb214e13754eb369ab2409
Daniel Pham <nguyen.pc@live.com>
2018-10-20 04:30:58 +0700

    send vcf to android intent

--- src/android/CordovaNativeContacts.java
+++ src/android/CordovaNativeContacts.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.net.Uri;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.File;
+import cordova.plugin.contact.FileProvider;
 
 public class CordovaNativeContacts extends CordovaPlugin {
 
@@ -29,14 +32,21 @@ public class CordovaNativeContacts extends CordovaPlugin {
         }
          if (action.equals("updateContact")) {
             Context context = this.cordova.getActivity().getApplication();
-            Intent intent = new Intent(Intent.ACTION_INSERT_OR_EDIT);
-            intent.setType(Contacts.CONTENT_ITEM_TYPE);
-
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            // intent.setType(Contacts.CONTENT_VCARD_TYPE);
             JSONObject params = args.getJSONObject(0);
-            addExtra(intent, params, "name", ContactsContract.Intents.Insert.NAME);
-            addExtra(intent, params, "phone", ContactsContract.Intents.Insert.PHONE);
-            addExtra(intent, params, "email", ContactsContract.Intents.Insert.EMAIL);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            File file = new File(this.stripFileProtocol(params.getString("path")));
+						Uri path = FileProvider.getUriForFile(context, cordova.getActivity().getPackageName() + ".opener.provider", file);
+
+            intent.setDataAndType(path,"text/vcard");
+
+            // addExtra(intent, params, "name", ContactsContract.Intents.Insert.NAME);
+            // addExtra(intent, params, "phone", ContactsContract.Intents.Insert.PHONE);
+            // addExtra(intent, params, "email", ContactsContract.Intents.Insert.EMAIL);
+            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+              intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NO_HISTORY);
+
             intent.putExtra("finishActivityOnSaveCompleted", true);
             context.startActivity(intent);
             return true;
@@ -50,4 +60,13 @@ public class CordovaNativeContacts extends CordovaPlugin {
             intent.putExtra(extraKey, value);
         }
     }
+
+    private String stripFileProtocol(String uriString) {
+		if (uriString.startsWith("file://")) {
+			uriString = uriString.substring(7);
+		} else if (uriString.startsWith("content://")) {
+			uriString = uriString.substring(10);
+		}
+		return uriString;
+	}
 }
